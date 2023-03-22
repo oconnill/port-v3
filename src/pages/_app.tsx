81237acabdb53644d9de927cad9f3e4157730192
danoconne11 <dan.oconnell@thekrazycouponlady.com>
2023-03-22 09:32:44 -0600

    use local font optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Open_Sans} from "@next/font/google";
+import { Open_Sans} from "next/font/google";
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
