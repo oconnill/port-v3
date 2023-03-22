a07f141ab254a2435a56466acdef5426e973f2c5
danoconne11 <dan.oconnell@thekrazycouponlady.com>
2023-03-22 09:48:14 -0600

    set up formatting and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Open_Sans} from "next/font/google";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Open_Sans } from "next/font/google";
 
-const openSans = Open_Sans({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ["latin"], variable: "--open-sans" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <main className={openSans.className}>
-    <Component {...pageProps} />
-  </main>
+  return (
+    <main className={openSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
