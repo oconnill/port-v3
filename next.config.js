3c42b240be55993c4877760ec483e929c3f742be
danoconne11 <dan.oconnell@thekrazycouponlady.com>
2023-07-16 14:56:04 -0600

    revert next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/port-v3/' : '',
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
