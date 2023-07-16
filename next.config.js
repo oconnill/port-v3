a45bbd6869aec44561e12f70059105cbf36f31f1
danoconne11 <dan.oconnell@thekrazycouponlady.com>
2023-07-16 14:53:04 -0600

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/port-v3/' : '',
 }
 
 module.exports = nextConfig
